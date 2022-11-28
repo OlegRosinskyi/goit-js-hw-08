b7883203b9dfef633c0018c8934d7e1954fe4164
OlegRosinskyi <olegrosinskyi@gmail.com>
2022-11-28 14:55:39 +0200

    update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+console.log('played the video!');
+//const Vimeo = require('@vimeo/player');
+//import Vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+console.log(Player);
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video!');
+});
+const videoplayerCT = getItem('videoplayer-current-time');
+console.log(videoplayerCT);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
